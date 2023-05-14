b11dd350f3aec076ff12ebbf3f3d2697417af9e9
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-05-14 18:46:43 +0200

    aaaaa

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ from flask import Flask, render_template, request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, upgrade
 from random import randint
-
 from model import db, seedData, Customer
+from appconfig.config import DevelopmentConfig, ProductionConfig
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:stefan@localhost/players0101'
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'database.sqlite')
+
+if os.getenv('RUNENVIRONMENT') == "Production":
+    app.config.from_object(ProductionConfig())
+else:
+    app.config.from_object(DevelopmentConfig())
+
+
 db.app = app 
 db.init_app(app)
 migrate = Migrate(app,db)
