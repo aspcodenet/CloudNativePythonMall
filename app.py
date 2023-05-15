edda6f2a88b946c3dd77daeac367f3f9c9af0e0a
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-05-15 16:53:21 +0200

    Conf + API

--- app.py
+++ app.py
@@ -2,21 +2,70 @@ from flask import Flask, render_template, request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, upgrade
 from random import randint
-
+from appconfig.config import DevelopmentConfig, ProductionConfig
 from model import db, seedData, Customer
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:stefan@localhost/players0101'
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'database.sqlite')
+
+if os.getenv('RUNENVIRONMENT') == "Production":
+    app.config.from_object(ProductionConfig())
+else:
+    app.config.from_object(DevelopmentConfig())
+
+
+# Go -> ORM -> GORM 
+# Python Flask, flask_sqlalchemy -> Sqlalchemy 
+# mysql / sqlite
+
 db.app = app 
 db.init_app(app)
 migrate = Migrate(app,db)
 
+@app.route("/api/customer/<id>")
+def apiCustomer(id):
+    c = Customer.query.filter_by(Id=id).first_or_404()
+    # select * from customer where id=@id
+    return jsonify({ "Id": c.Id, 
+                 "Name":c.Name, 
+                 "City":c.City,
+                  "TelephoneCountryCode":c.TelephoneCountryCode,
+                   "Telephone":c.Telephone })
 
-@app.route("/api/customers")
+
+@app.route("/api/customer/<id>", methods=["PUT"])
+def apiCustomerUpdate(id):
+    data = request.get_json()
+    c = Customer.query.filter_by(Id=id).first_or_404()
+    c.City = data["City"]
+    c.Name = data["Name"]
+    c.Telephone = data["Telephone"]
+    c.TelephoneCountryCode = data["TelephoneCountryCode"]
+    db.session.commit()
+    return jsonify({ "Id": c.Id, 
+                 "Name":c.Name, 
+                 "City":c.City,
+                  "TelephoneCountryCode":c.TelephoneCountryCode,
+                   "Telephone":c.Telephone }), 200
+
+@app.route("/api/customer", methods=["POST"])
+def apiCustomerCreate():
+    data = request.get_json()
+    c = Customer()
+    c.City = data["City"]
+    c.Name = data["Name"]
+    c.Telephone = data["Telephone"]
+    c.TelephoneCountryCode = data["TelephoneCountryCode"]
+    db.session.add(c)
+    db.session.commit()
+    return jsonify({ "Id": c.Id, 
+                 "Name":c.Name, 
+                 "City":c.City,
+                  "TelephoneCountryCode":c.TelephoneCountryCode,
+                   "Telephone":c.Telephone }), 201
+
+
+@app.route("/api/customer")
 def apiCustomers():
     lista = []
     for c in Customer.query.all():
